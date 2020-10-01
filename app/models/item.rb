0e22a337c620d27d0e88e59cd300ed4541aa2574
yama0903 <y.tm.0215@gmail.com>
2020-10-01 13:37:43 +0900

    不要アソシエーションのコメントアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
 
   belongs_to :user
-  has_many :comments
+  # has_many :comments
   has_one :order
   has_one_attached :image
 
